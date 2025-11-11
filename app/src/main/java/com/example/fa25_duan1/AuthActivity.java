36002090805394362030b32642a863f00e7ab463
Thiên Hạo . <lieuthienhao2006@gmail.com>
2025-11-11 21:54:49 +0700

    test push

--- app/src/main/java/com/example/fa25_duan1/AuthActivity.java
+++ app/src/main/java/com/example/fa25_duan1/AuthActivity.java
@@ -21,6 +21,7 @@ public class AuthActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String hello = "hello";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
