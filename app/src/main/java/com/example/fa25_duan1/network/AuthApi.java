59db50135d40461a72d94932fbb2151543d5f903
Thiên Hạo . <lieuthienhao2006@gmail.com>
2025-12-16 06:41:44 +0700

    LAST UPDATE 16/12/2025

--- app/src/main/java/com/example/fa25_duan1/network/AuthApi.java
+++ app/src/main/java/com/example/fa25_duan1/network/AuthApi.java
@@ -15,6 +15,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
@@ -41,7 +42,7 @@ public interface AuthApi {
     @POST("auth/logout")
     Call<ApiResponse<RefreshTokenResponse>> logout(@Body RefreshTokenRequest request);
 
-    @POST("auth/me")
+    @GET("auth/me")
     Call<ApiResponse<User>> getMyInfo();
 
     @Multipart
